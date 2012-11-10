582f81548d0669416e90ea572fdbbab5c9200b0e
Kike Cabrera <kikecalpe@gmail.com>
2012-11-10 12:06:00 +0100

    Added and removed files to clean publish.

---
+++ Serp/src/es/dokansoft/gdx/serp/model/Poo.java
@@ -0,0 +1,10 @@
+package es.dokansoft.gdx.serp.model;
+
+public class Poo extends SnakePart{
+	public float timer;
+
+	public Poo(int x, int y, float timer){
+		super(x,y);
+		this.timer = timer;
+	}
+}
